dd4a0a92c32a7cead68b1477893a0420ad435667
miguelparra22 <orjuelaparra@gmail.com>
2022-12-29 22:01:38 -0500

    avance diseño, login y dashboard

---
+++ Assets/functions/validations.js
@@ -0,0 +1,28 @@
+/*Que el campo de texto solo acepte letras*/
+
+
+
+
+/*Que el campo de email, solo acepte emails*/
+
+
+
+
+/*Que el campo de edad solo acepte numero y ademas tenga un rango de edades.*/
+
+
+
+
+
+/*Crear una contraseña segura o verificar que esta sea segura*/
+
+
+
+
+/*Ningun campo vacio*/
+
+
+
+
+/** validar la edad que no sea menor a 18 ni mayor a 65 */
+
